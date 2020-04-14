8b7584ee9ff49edeaf295a6e93306703c8dab299
tomiyee <tommy.and.heng@gmail.com>
2020-04-14 00:45:54 -0400

    Loading Service Account from Media FIles

--- appinventor/components/src/com/google/appinventor/components/runtime/GoogleSheets.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/GoogleSheets.java
@@ -1,4 +1,4 @@
-// -*- mode: java; c-basic-offset: 2; -*-
+ // -*- mode: java; c-basic-offset: 2; -*-
 // Copyright 2009-2011 Google, All Rights reserved
 // Copyright 2011-2012 MIT, All rights reserved
 // Released under the Apache License, Version 2.0
@@ -12,6 +12,9 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.http.HttpTransport;
+import com.google.appinventor.components.annotations.androidmanifest.ActivityElement;
+import com.google.appinventor.components.annotations.androidmanifest.IntentFilterElement;
+import com.google.appinventor.components.annotations.androidmanifest.ActionElement;
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.DesignerProperty;
 import com.google.appinventor.components.annotations.PropertyCategory;
@@ -19,15 +22,21 @@ import com.google.appinventor.components.annotations.SimpleEvent;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.annotations.SimpleProperty;
+import com.google.appinventor.components.annotations.UsesActivities;
 import com.google.appinventor.components.annotations.UsesLibraries;
 import com.google.appinventor.components.annotations.UsesPermissions;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.PropertyTypeConstants;
 import com.google.appinventor.components.common.YaVersion;
+import com.google.appinventor.components.runtime.WebViewActivity;
 import com.google.appinventor.components.runtime.util.ClientLoginHelper;
 import com.google.appinventor.components.runtime.util.ErrorMessages;
 import com.google.appinventor.components.runtime.util.IClientLoginHelper;
+import com.google.appinventor.components.runtime.util.MediaUtil;
+import com.google.appinventor.components.runtime.util.FileUtil;
+import com.google.appinventor.components.runtime.util.AsyncCallbackPair;
 import com.google.appinventor.components.runtime.util.AsynchUtil;
+import com.google.appinventor.components.runtime.util.WebServiceUtil;
 import com.google.appinventor.components.runtime.util.YailList;
 
 import com.google.api.client.auth.oauth2.Credential;
@@ -39,6 +48,8 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
@@ -47,17 +58,27 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
 import java.lang.Math;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Arrays;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Appinventor Google Sheets Component
@@ -78,13 +99,25 @@ import java.util.List;
     "android.permission.READ_EXTERNAL_STORAGE")
 @UsesLibraries(libraries =
   "googlesheets.jar," +
+  "jackson-core.jar," +
   "google-api-client.jar," +
+  "google-api-client-jackson2.jar," +
   "google-http-client.jar," +
   "google-http-client-jackson2.jar," +
   "google-oauth-client.jar," +
   "google-oauth-client-jetty.jar," +
   "jetty.jar," +
   "jetty-util.jar")
+@UsesActivities(activities = {
+    @ActivityElement(name = "com.google.appinventor.components.runtime.WebViewActivity",
+       configChanges = "orientation|keyboardHidden",
+       screenOrientation = "behind",
+       intentFilters = {
+           @IntentFilterElement(actionElements = {
+               @ActionElement(name = "android.intent.action.MAIN")
+           })
+    })
+})
 public class GoogleSheets extends AndroidNonvisibleComponent implements Component {
   // private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
   // private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
@@ -93,25 +126,152 @@ public class GoogleSheets extends AndroidNonvisibleComponent implements Componen
   // // The Scopes and the Credentials File Path.
   // private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
   // private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+  private static final String LOG_TAG = "GOOGLE SHEETS";
 
+  private static final String WEBVIEW_ACTIVITY_CLASS = WebViewActivity.class
+    .getName();
+  private int requestCode;
   public GoogleSheets(ComponentContainer componentContainer) {
     super(componentContainer.$form());
+    this.container = componentContainer;
+    this.activity = componentContainer.$context();
   }
 
   /* Getter and Setters for Properties */
-  private String apiKey = "";
+  private String apiKey;
+  private String credentialsPath;
+  private File cachedCredentialsFile = null;
+  private String tokensPath;
+  private String spreadsheetID = "1q0sM8BeBRL2n6EHEkkB54WbKn6pT-boChEbS3HzNe_g";
 
+  //   private final Activity activity;
+  private final ComponentContainer container;
+  private final Activity activity;
+
+  //   private final IClientLoginHelper requestHelper;
   @SimpleProperty(
     description = "API Key")
   public String ApiKey() {
     return apiKey;
   }
 
-  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING, defaultValue = "")
+  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING,
+    defaultValue = "")
   @SimpleProperty
   public void ApiKey(String apiKey) {
     this.apiKey = apiKey;
   }
+
+  @SimpleProperty(
+    description = "The Credentials JSON file")
+  public String credentialsJson() {
+    return credentialsPath;
+  }
+
+  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_ASSET,
+    defaultValue = "")
+  @SimpleProperty
+  public void credentialsJson (String credentialsPath) {
+    this.credentialsPath = credentialsPath;
+  }
+
+
+  @SimpleProperty(
+    description = "The ID you can find in the URL of the Google Sheets you want to edit")
+  public String spreadsheetID() {
+    return spreadsheetID;
+  }
+
+  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING,
+    defaultValue = "")
+  @SimpleProperty(description="The ID for the Google Sheets file you want to edit. " +
+    "You can find the spreadsheetID in the URL of the Google Sheets file.")
+  public void spreadsheetID(String spreadsheetID) {
+    this.spreadsheetID = spreadsheetID;
+  }
+
+  /* Temporary testing functions */
+  String APPLICATION_NAME = "Test Application";
+
+  private Credential authorize() throws IOException, GeneralSecurityException {
+
+    InputStream is = GoogleSheets.class
+      .getResourceAsStream(this.credentialsPath);
+
+    Credential credential = GoogleCredential.fromStream(is)
+      .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+
+    // InputStream in = GoogleSheets.class.getResourceAsStream(this.credentialsPath);
+    // GoogleClientSecrets clientSecrets = GoogleClientSecrets.load (
+    //   JacksonFactory.getDefaultInstance(),
+    //   new InputStreamReader(in)
+    // );
+    //
+    // List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
+    // // This is what opens the Google OAuth things in a new window thing.
+    // GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+    //   GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(),
+    //   clientSecrets, scopes)
+    //   .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
+    //   .setAccessType("offline")
+    //   .build();
+    //
+    // Credential credential = new AuthorizationCodeInstalledApp(
+    //   flow, new LocalServerReceiver())
+    //   .authorize("user");
+
+    return credential;
+  }
+
+  public Sheets getSheetsService ()  throws IOException, GeneralSecurityException {
+    Credential credential = authorize();
+    return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+      JacksonFactory.getDefaultInstance(), credential)
+      .setApplicationName(APPLICATION_NAME)
+      .build();
+  }
+
+  @SimpleFunction()
+  public String TestGet () throws IOException, GeneralSecurityException {
+
+    // TODO - Only change cachedCredentialsFile if credentials path is different. See FusionTables
+    Sheets sheets = getSheetsService();
+    
+    return "Okay";
+
+    // Sheets sheetsService = getSheetsService();
+
+    //
+    // String range = "congress!A2:F10";
+    // return "Checkpoint 2 Reached";
+    //
+    // ValueRange response = sheetsService.spreadsheets().values()
+    //   .get(spreadsheetID, range)
+    //   .execute();
+    //
+    // List<List<Object>> values = response.getValues();
+    //
+    // if (values == null || values.isEmpty()) {
+    //   return "No Data Found.";
+    // }
+    // // Just prints the first thing in the first list
+    // else {
+    //   String res = "";
+    //   for (List row : values) {
+    //     String s = String.format("%s %s from %s", row.get(5), row.get(4), row.get(1));
+    //     res += s;
+    //     res += ";";
+    //   }
+    //   return res;
+    // }
+  }
+
+
+  @SimpleEvent
+  public void GotText(String response) {
+    EventDispatcher.dispatchEvent(this, "GotText", response);
+  }
+
 
   /* Helper Functions for the User */
 
@@ -161,7 +321,7 @@ public class GoogleSheets extends AndroidNonvisibleComponent implements Componen
 
   @SimpleEvent
   public void GotRowData (YailList rowDataList) {
-    EventDispatcher.dispatchEvent(this, "ReadRow", rowDataList);
+    EventDispatcher.dispatchEvent(this, "GotRowData", rowDataList);
   }
 
   @SimpleFunction
