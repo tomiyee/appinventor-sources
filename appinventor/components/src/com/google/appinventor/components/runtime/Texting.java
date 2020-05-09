feaf38353a59528f18a98839ef196377662a4e78
tomiyee <tommy.and.heng@gmail.com>
2020-05-09 01:05:23 -0400

    Ref "guava.jar" lib file. Import guava-27.1-android.jar

--- appinventor/components/src/com/google/appinventor/components/runtime/Texting.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Texting.java
@@ -165,7 +165,7 @@ import android.widget.Toast;
   "google-http-client-android2-beta.jar," +
   "google-http-client-android3-beta.jar," +
   "google-oauth-client.jar," +
-  "guava-14.0.1.jar")
+  "guava.jar")
 public class Texting extends AndroidNonvisibleComponent
   implements Component, OnResumeListener, OnPauseListener, OnInitializeListener, OnStopListener,
     Deleteable, ActivityResultListener {
